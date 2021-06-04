c0f4ffdd34792639c6c64f51ab77fb68cf4ffe8e
vic <vic@DESKTOP-K1G89T6>
2021-06-04 21:32:37 +0800

    JavaWeb

--- src/main/java/com/Web/controller/HellowGenericServlet.java
+++ src/main/java/com/Web/controller/HellowGenericServlet.java
@@ -7,13 +7,15 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
+//請求資訊(URL):http://localhost:8080/JavaWeb0531/hi?name=John(透過網址取得參數(?name=John))
+// /hi                ->url-pattern  ,要到WEB-INF/web.xml 中設定配置
+//JavaWeb0531->context.xml ,要到META-INF/context.xml 中設定配置
 public class HellowGenericServlet  extends GenericServlet{
 
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-       //req表示收到client的請求資訊  http://localhost:8080/..../.....?name=John(透過網址取得參數(?name=John))
-       String name = req.getParameter("name");
+@Override
+public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+ //req表示收到client的請求資訊  
+String name = req.getParameter("name");
 //res表示要回應給client端的內容
   System.out.printf("Hi %s %s\n",name,new Date());//印在Server的consle中(通常用於debug或觀察變數)
    PrintWriter out= res.getWriter();//透過out可以將訊息回傳給client
