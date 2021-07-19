61ec1aadcd6ac22a2b4a151d367a03cc75f94bc5
vic <vic@192.168.0.52>
2021-07-19 21:27:00 +0800

    JavaWeb

---
+++ src/main/java/com/sql/alter_book.sql
@@ -0,0 +1,3 @@
+--增加一個 amount (數量) int 欄位
+alter table Book add column amount int not null default 0;
+
---
+++ src/main/java/com/sql/selectbook.sql
@@ -0,0 +1,4 @@
+select * from Book;
+select name from Book;
+select b.ID, b.TS, b.PRICE, b."NAME" from Book b;
+
