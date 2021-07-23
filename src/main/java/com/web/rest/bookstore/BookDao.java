842aac725faa8edc9e1c7c7976e5df5c54495e35
vic <vic@192.168.0.52>
2021-07-23 19:50:16 +0800

    JavaWeb

--- src/main/java/com/web/rest/bookstore/BookDao.java
+++ src/main/java/com/web/rest/bookstore/BookDao.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookDao {
-    
+
     // 書籍資料庫
     public static List<Book> books = new ArrayList<>();
     // 資料庫連線物件
     private static Connection conn;
-    
+
     static {
         try {
             // 資料庫驅動物件
@@ -27,13 +27,13 @@ public class BookDao {
         } catch (Exception e) {
         }
     }
-    
+
     // 多筆查詢
     public static List<Book> getBooks() {
         books.clear();
         String sql = "SELECT id, name, price, amount, ts FROM Book";
-        try(Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);) {
+        try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);) {
             // 所抓到的每一筆紀錄，要注入到 book 物件中存放
             while (rs.next()) {
                 Book book = new Book();
@@ -44,56 +44,63 @@ public class BookDao {
                 // 加入到 books 集合中
                 books.add(book);
             }
-            
+
         } catch (Exception e) {
         }
-        
+
         return books;
     }
-    
+
     // 單筆查詢
     public static Book getBook(Integer id) {
         return books.stream()
                 .filter(b -> b.getId() == id)
                 .findFirst()
                 .get();
-    } 
-    
+    }
+
     // 新增
     public static Boolean createBook(Book book) {
         boolean flag = books.stream()
                 .filter(b -> b.getId() == book.getId())
                 .findAny()
                 .isPresent();
-        if(flag == false) {
+        if (flag == false) {
             books.add(book);
             return true;
         }
         return false;
     }
-    
+
     // 修改
     public static Boolean updateBook(Integer id, Book book) {
         // 是否庫存中有此筆資料 ?
         Book oBook = getBook(id);
-        if(oBook == null) {
+        if (oBook == null) {
             return false;
         }
         // 將 book 的資料更新到資料表中
         String sql = "Update Book Set name=? , price=? , amount=? where id=?";
-        try(PreparedStatement pstmt =conn.prepareStatement(sql)) {
-            
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, book.getName());
+            pstmt.setInt(2, book.getPrice());
+            pstmt.setInt(3, book.getAmount());
+            //執行更新
+            int rowcount = pstmt.executeUpdate(); //設id 必有一個資料受影響
+            return rowcount == 1 ? true : false;
         } catch (Exception e) {
+            e.printStackTrace(System.out);
+            return false;
         }
-        
-        return true;
+
     }
-    
+
     // 刪除
     public static Boolean deleteBook(Integer id) {
         // 是否庫存中有此筆資料 ?
         Book oBook = getBook(id);
-        if(oBook == null) {
+        if (oBook == null) {
             return false;
         }
         books.remove(oBook);
