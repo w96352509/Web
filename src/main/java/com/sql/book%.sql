681a5487b0610aa86067420ee67ce5fcbd40ec4f
vic <vic@192.168.0.52>
2021-07-24 04:14:04 +0800

    JavaWeb

--- src/main/java/com/sql/book%.sql
+++ src/main/java/com/sql/book%.sql
@@ -1,9 +1,17 @@
 -- 計算每種書籍價值, 佔比
-SELECT b."NAME", SUM(b.PRICE * b.AMOUNT) as subtotal, 
+SELECT b."NAME", 
+sum(b.PRICE) as total,
+SUM(b.PRICE * b.AMOUNT) as subtotal, 
+sum(b.PRICE) / sum(b.PRICE*b.AMOUNT),
 SUM(b.PRICE * b.AMOUNT) as percent
-
 FROM BOOK b
+
 GROUP BY b."NAME";
+
+
+
+
+
 
 -- 全部書籍總價值 ?
 select SUM(b.PRICE * b.AMOUNT) as total from book b
